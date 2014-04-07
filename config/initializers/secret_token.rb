4268f836cf9295194688b4dae2bb5fab430dcfe4
vagrant <vagrant@student-dev-box>
2014-04-07 15:15:28 +0000

    Inital Commit Dude, Really

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DudeReally::Application.config.secret_key_base = 'c3e6f30be09ad691a5ebba6e20d5f14276fe78c5653dec2443888ee8ff1d53c3d9130fa5f922e8778b774d97fdd57166605118ecc48497913770925c5bde6e3a'
