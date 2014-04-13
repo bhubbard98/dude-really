677b74f4c81a8f089d44bbb2a6d9d83f509984ec
Brad Hubbard <brad.hubbard77@gmail.com>
2014-04-13 17:02:44 -0500

    User submission restrictions added but no alert has been added

--- app/models/dumblaw.rb
+++ app/models/dumblaw.rb
@@ -1,3 +1,6 @@
 class Dumblaw < ActiveRecord::Base
 	acts_as_voteable
+	validates :title, :length => { :minimum => 4 }
+	validates :description, :length => { :maximum => 140 }
+	validates :source, :uniqueness => true
 end
