c6c6b39728f99bd08340098cd2ea67dbd6086e5c
Brad Hubbard <brad.hubbard77@gmail.com>
2014-04-14 12:00:21 -0500

    more mailer stuff

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,9 @@
 class UserMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "no-reply@example.com"
 
   def threshold_mail(email, law)
-  	
+  	@email = email
+  	@law = 
+  	mail(to: @email.email, subject: 'This is a dumblaw that should be reexamined.')
   end
 end
