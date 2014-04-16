4d8dc32d894d9a56e1078ce58eef677392e65617
Brad Hubbard <brad.hubbard77@gmail.com>
2014-04-16 15:35:49 -0500

    Fix to db and intial code for JS on homepage

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,12 @@
+// # Place all the behaviors and hooks related to the matching controller here.
+// # All this logic will automatically be available in application.js.
+// # You can use CoffeeScript in this file: http://coffeescript.org/
+
+// $('.large-12').ready(function(){
+// 	alert("HEY");
+// })
+// var laws = ['dumblaw1','dumblaw10', 'dumblaw12', 'dumblaw36', 'dumblaw41']
+// function dumbLaw (){
+// 	var random_law = gon.laws[Math.floor(Math.random() * gon.laws.length)];
+// 	var rand = $('')
+// }
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,4 @@
 class HomeController < ApplicationController
   def index
-  	gon.laws = [DumbLaw.find(1), DumbLaw.find(10), DumbLaw.find(12), DumbLaw.find(36), DumbLaw.find(41)]
   end
 end
