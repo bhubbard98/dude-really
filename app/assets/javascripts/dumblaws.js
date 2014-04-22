b57fccf0bb39bc6649376c2ca7015b02658ba94e
Brad Hubbard <brad.hubbard77@gmail.com>
2014-04-22 14:40:32 -0500

    I missed this somehow

--- app/assets/javascripts/dumblaws.js
+++ app/assets/javascripts/dumblaws.js
@@ -2,8 +2,3 @@
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
 
-var laws = ['dumblaw1','dumblaw10', 'dumblaw12', 'dumblaw36', 'dumblaw41']
-function dumbLaw (){
-	var random_law = gon.laws[Math.floor(Math.random() * gon.laws.length)];
-	var rand = $('')
-}
