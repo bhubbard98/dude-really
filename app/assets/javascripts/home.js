ca27317cd0954228f2eee03bd83abacf5952590f
Brad Hubbard <brad.hubbard77@gmail.com>
2014-04-23 09:52:32 -0500

    placing in gon gem

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -2,11 +2,14 @@
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
 
-// $('.large-12').ready(function(){
-// 	alert("HEY");
-// })
-// var laws = ['dumblaw1','dumblaw10', 'dumblaw12', 'dumblaw36', 'dumblaw41']
-// function dumbLaw (){
-// 	var random_law = gon.laws[Math.floor(Math.random() * gon.laws.length)];
-// 	var rand = $('')
-// }
+$('.large-12').ready(function(){
+	// alert("HEY");
+})
+var laws = ['dumblaw1','dumblaw10', 'dumblaw12', 'dumblaw36', 'dumblaw41']
+function dumbLaw (){
+	var random_law = gon.laws[Math.floor(Math.random() * gon.laws.length)];
+	var $rand = $('div#laws');
+}
+$(function () {
+    setInterval(laws, 15000);
+});
