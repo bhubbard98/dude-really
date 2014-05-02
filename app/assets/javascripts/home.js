bd46786edf70ae3270998bbc4ac369e64c1c03c5
Brad Hubbard <brad.hubbard77@gmail.com>
2014-05-02 14:49:17 -0700

    Alright. I rolled back to a previous commit then added the JS to the homepage. BOOM!

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -2,11 +2,18 @@
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
 
-// $('.large-12').ready(function(){
-// 	alert("HEY");
-// })
-// var laws = ['dumblaw1','dumblaw10', 'dumblaw12', 'dumblaw36', 'dumblaw41']
-// function dumbLaw (){
-// 	var random_law = gon.laws[Math.floor(Math.random() * gon.laws.length)];
-// 	var rand = $('')
-// }
+// Array of 5 different laws
+var laws = ['Unlawful bear exploitation is a Class B felony and is punishable as provided by law','A referral is needed to get medical care for kleptomania', 'Fake wrestling is prohibited', 'It is illegal to cause a catastrophe', 'Cannot shoot fish with a gun']
+
+// When called, generate a random number between 0 and 4
+// Use that number to access the index of the laws array
+
+var dumbLaw = function (){
+	var random_law = Math.floor(Math.random() * 5);
+    console.log(random_law);
+	$("#theLaws").text(laws[random_law]);
+};
+
+$(document).ready(function() {
+    setInterval(dumbLaw, 3000);
+});
